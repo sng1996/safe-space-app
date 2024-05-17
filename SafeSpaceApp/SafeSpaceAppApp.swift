3b059627d98579cdfef62adb3e2bae7f8198ed48
sergey.gavrilko <sergey.gavrilko@aliexpress.ru>
2024-05-18 00:39:44 +0300

    create project

---
+++ SafeSpaceApp/SafeSpaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSpaceAppApp.swift
+//  SafeSpaceApp
+//
+//  Created by Sergey Gavrilko on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SafeSpaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
